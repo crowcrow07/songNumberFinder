c79b1f1fb86a30c215c4dff0af48e2242b87fd3d
crowcrow07 <skathd3402@gmail.com>
2024-04-22 10:36:09 +0900

    feat : 타이틀 컴포넌트

---
+++ src/app/components/header/title/Title.tsx
@@ -0,0 +1,8 @@
+export default function Title() {
+  return (
+    <div className="text-center">
+      <h1 className="font-bold text-5xl mb-4">이 노래 노래방에 있어?</h1>
+      <h2 className="text-xl mb-[25px] text-[#d9d9d9]">노래방 번호 검색기</h2>
+    </div>
+  );
+}
