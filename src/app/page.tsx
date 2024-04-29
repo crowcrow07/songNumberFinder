6ee5e74e1749198615a6612f977fe7ff114008b2
crowcrow07 <skathd3402@gmail.com>
2024-04-29 12:41:39 +0900

    feat : import resultbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header";
+import ResultsBox from "@/components/results_box";
 
 export default function Home() {
   return (
     <main className="w-screen flex justify-center h-screen overflow-y-scroll">
       <div className="xl:w-[1280px] lg:w-[1024px] md:w-[768px] sm:w-[640px] w-[80%]  flex gap-16 flex-col items-center justify-center bg-gray-50">
         <Header />
+        <ResultsBox />
       </div>
     </main>
   );
