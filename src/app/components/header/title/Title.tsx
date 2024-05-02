a3dedb764697dd510397bc92cca21d9e2885768c
crowcrow07 <skathd3402@gmail.com>
2024-05-02 12:29:17 +0900

    feat : title css 반응형

--- src/app/components/header/title/Title.tsx
+++ src/app/components/header/title/Title.tsx
@@ -1,8 +1,12 @@
 export default function Title() {
   return (
     <div className="text-center">
-      <h1 className="font-bold text-5xl mb-4">이 노래 노래방에 있어?</h1>
-      <h2 className="text-xl mb-[25px] text-[#d9d9d9]">노래방 번호 검색기</h2>
+      <h1 className="font-bold xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl mb-4">
+        이 노래 노래방에 있어?
+      </h1>
+      <h2 className="xl:text-xl lg:text-lg md:text-base text-sm mb-[25px] text-[#d9d9d9]">
+        노래방 번호 검색기
+      </h2>
     </div>
   );
 }
