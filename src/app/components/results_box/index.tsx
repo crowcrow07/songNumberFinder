6e01fccdd8292a50737a2f1e95782219ced5a6c6
crowcrow07 <skathd3402@gmail.com>
2024-05-02 18:43:05 +0900

    feat : 검색 키워드 받기

--- src/app/components/results_box/index.tsx
+++ src/app/components/results_box/index.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import ResultCard from "../result_card";
+import { useRecoilValue } from "recoil";
+import { searchKeywordList, searchKeywordInput } from "@/app/recoil/atom/atom";
+
+import { TjSongType } from "@/app/api/api";
+
 export default function ResultsBox() {
+  const searchKeywordArray: TjSongType[] = useRecoilValue(searchKeywordList);
+  const searchKeywordInputValue: string = useRecoilValue(searchKeywordInput);
+
+  console.log(searchKeywordArray);
+
   return (
-    <div className="pt-[36px] px-[56px] drop-shadow-xl flex flex-col gap-2 rounded-[20px] h-96 xl:w-[500px] lg:w-[460px] md:w-[420px] sm:-[400px] w-[300px] bg-white">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+    <div className="pt-[36px] xl:px-[56px] lg:px-[40px] md:px-[28px] sm:px-[24px] px-[16px] drop-shadow-xl flex flex-col items-center  rounded-[20px] h-[500px] xl:w-[500px] lg:w-[460px] md:w-[420px] sm:w-[400px] w-[320px] bg-white pb-4">
+      <div className="text-[24px]">
+        {searchKeywordInputValue} 로 검색된 결과
+      </div>
+      <div className="overflow-y-scroll w-full p-2">
+        <div className="gap-4 flex flex-col w-full">
+          {searchKeywordArray &&
+            searchKeywordArray.map((keyword, idx) => {
+              return <ResultCard key={keyword.id} data={keyword} />;
+            })}
+        </div>
+      </div>
     </div>
   );
 }
