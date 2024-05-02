2d2e694c0faf1078afbfb2b82af9c821c4c8cccf
crowcrow07 <skathd3402@gmail.com>
2024-05-02 18:41:53 +0900

    feat : search get api

---
+++ src/app/api/db/[search_keyword]/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { searchDbExecuteQuery } from "../executeQuery";
+
+type Params = {
+  search_keyword: string;
+};
+
+export async function GET(req: Request, context: { params: Params }) {
+  try {
+    const searchKeyword = context.params.search_keyword;
+    const query = `SELECT * FROM TJ t WHERE REGEXP_REPLACE(title, '\\s+', '') REGEXP REGEXP_REPLACE('${searchKeyword}', '\\s+', '');`;
+
+    const results = await searchDbExecuteQuery(query);
+
+    return NextResponse.json({ results: results }, { status: 200 });
+  } catch (error) {
+    console.error("검색 실패", error);
+    return NextResponse.json({ error: "검색 실패" }, { status: 500 });
+  }
+}
