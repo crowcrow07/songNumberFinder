e6f0556ef494f3304af12055c753c4398ee17f28
crowcrow07 <skathd3402@gmail.com>
2024-05-02 12:29:30 +0900

    feat : resultbox component

--- src/app/components/results_box/index.tsx
+++ src/app/components/results_box/index.tsx
@@ -1,1 +1,9 @@
-export default function ResultsBox() {}
+export default function ResultsBox() {
+  return (
+    <div className="pt-[36px] px-[56px] drop-shadow-xl flex flex-col gap-2 rounded-[20px] h-96 xl:w-[500px] lg:w-[460px] md:w-[420px] sm:-[400px] w-[300px] bg-white">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
+}
