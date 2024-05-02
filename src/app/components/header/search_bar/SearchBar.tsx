b056b16886c3c785e88b5f0c420b8c99ce2c850a
crowcrow07 <skathd3402@gmail.com>
2024-05-02 12:29:00 +0900

    feat : css 반응형 searchbar

--- src/app/components/header/search_bar/SearchBar.tsx
+++ src/app/components/header/search_bar/SearchBar.tsx
@@ -3,9 +3,9 @@ import search from "@/app/assets/images/svg/search.svg";
 
 export default function SearchBar() {
   return (
-    <div className="bg-white w-[540px] h-[96px] rounded-[20px] flex justify-between px-[16px] py-4">
+    <div className="bg-white drop-shadow-xl xl:w-[540px] lg:w-[500px] md:w-[460px] sm:w-[420px] w-[320px] h-[96px] rounded-[20px] flex justify-between px-[16px] py-4">
       <input
-        className="flex-1 text-[24px]"
+        className="flex-1 xl:text-[24px] lg:text-[20px] md:text-[16px] sm:text-[12px]"
         type="text"
         placeholder="노래 제목 입력"
       />
