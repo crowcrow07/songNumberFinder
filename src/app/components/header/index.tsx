9c0f3f76ad88afe694592da8bbd2808b8f559114
crowcrow07 <skathd3402@gmail.com>
2024-05-06 13:07:44 +0900

    feat : header style 적용

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -3,7 +3,7 @@ import SearchBar from "./search_bar/SearchBar";
 
 export default function Header() {
   return (
-    <header className="">
+    <header className="xl:mt-[60px] lg:mt-[80px] md:mt-[100px] sm:mt-[100px] mt-[100px]">
       <Title />
       <SearchBar />
     </header>
