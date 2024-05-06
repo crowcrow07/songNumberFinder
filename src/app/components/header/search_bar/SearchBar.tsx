65b56c4d243e0d316402c89191e1e1f8c3041851
crowcrow07 <skathd3402@gmail.com>
2024-05-06 15:38:20 +0900

    fixed : 색깔 임시 고정

--- src/app/components/header/search_bar/SearchBar.tsx
+++ src/app/components/header/search_bar/SearchBar.tsx
@@ -38,7 +38,7 @@ export default function SearchBar() {
   return (
     <div className="bg-white drop-shadow-xl xl:w-[540px] lg:w-[500px] md:w-[460px] sm:w-[420px] w-[320px] h-[96px] rounded-[20px] flex justify-between px-[16px] py-4">
       <input
-        className="flex-1 xl:text-[24px] lg:text-[20px] md:text-[16px] sm:text-[12px] bg-white"
+        className="flex-1 xl:text-[24px] lg:text-[20px] md:text-[16px] sm:text-[12px] bg-white text-black"
         type="text"
         placeholder="노래 제목 입력"
         onChange={searchInputHandler}
