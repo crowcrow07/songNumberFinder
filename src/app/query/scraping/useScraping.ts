abf9901feb2950a6458fb9296e455098186b2aee
crowcrow07 <skathd3402@gmail.com>
2024-05-06 13:07:33 +0900

    feat : tj new song list usequery

--- src/app/query/scraping/useScraping.ts
+++ src/app/query/scraping/useScraping.ts
@@ -1,9 +1,20 @@
-import { TjSongListParams, getTjSongList } from "@/app/api/api";
+import {
+  TjSongListParams,
+  getTjNewSongList,
+  getTjSongList,
+} from "@/app/api/api";
 import { useQuery } from "@tanstack/react-query";
 
 export const useTjScrapingQuery = ({ text, size }: TjSongListParams) => {
   return useQuery({
     queryKey: [`tjScrapingQuery${text}${size}`],
     queryFn: () => getTjSongList({ text, size }),
+  });
+};
+
+export const useTjNewSongScrapingQuery = () => {
+  return useQuery({
+    queryKey: ["tjNewSongScrapingQuery"],
+    queryFn: () => getTjNewSongList(),
   });
 };
