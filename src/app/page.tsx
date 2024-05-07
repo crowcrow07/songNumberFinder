780913612bfe334569b411c4322ea4ef76dedf0c
crowcrow07 <skathd3402@gmail.com>
2024-05-07 18:57:16 +0900

    test : 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/header";
 import ResultsBox from "@/components/results_box";
 
 export default function Home() {
-  console.log(process.env.DB_HOST);
   return (
     <main className="w-screen h-screen overflow-y-scroll flex justify-center bg-[#ebebeb]">
       <div className="xl:w-[1280px] lg:w-[1024px] md:w-[768px] sm:w-[640px] w-[80%] flex gap-16 flex-col items-center justify-center min-h-[680px]">
