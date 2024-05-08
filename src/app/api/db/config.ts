c14826c9d5c549024d6e17af35ce46dcff74fe54
crowcrow07 <skathd3402@gmail.com>
2024-05-08 14:00:44 +0900

    fixed : 불필요한 console.log 삭제

--- src/app/api/db/config.ts
+++ src/app/api/db/config.ts
@@ -1,9 +1,5 @@
 import { createPool } from "mysql2/promise";
 
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_DATABASE);
 export const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
