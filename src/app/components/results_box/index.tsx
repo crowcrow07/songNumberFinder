18164eed42dae89c5c5a1b48056548479e3a51ad
crowcrow07 <skathd3402@gmail.com>
2024-05-08 14:03:34 +0900

    refactor : func 분리

--- src/app/components/results_box/index.tsx
+++ src/app/components/results_box/index.tsx
@@ -7,20 +7,38 @@ import { searchKeywordList, searchKeywordInput } from "@/app/recoil/atom/atom";
 import { TjSongDBType } from "@/app/types/type";
 
 export default function ResultsBox() {
-  const searchKeywordArray: TjSongDBType[] = useRecoilValue(searchKeywordList);
+  const searchKeywordArray: TjSongDBType[] | [] =
+    useRecoilValue(searchKeywordList);
   const searchKeywordInputValue: string = useRecoilValue(searchKeywordInput);
+
+  const isSearchKeywordInputValue = (value: string) => {
+    switch (value) {
+      case "":
+        return "검색어를 입력해주세요.";
+      default:
+        return `${searchKeywordInputValue} 로 검색된 결과`;
+    }
+  };
+
+  const isSearchKeywordArray = (array: TjSongDBType[] | []) => {
+    switch (array) {
+      case []:
+        return <div className="text-center">검색된 결과가 없습니다.</div>;
+      default:
+        return searchKeywordArray.map((keyword) => {
+          return <ResultCard key={keyword.id} data={keyword} />;
+        });
+    }
+  };
 
   return (
     <div className="pt-[36px] xl:px-[56px] lg:px-[40px] md:px-[28px] sm:px-[24px] px-[16px] drop-shadow-xl flex flex-col items-center  rounded-[20px] h-[500px] xl:w-[500px] lg:w-[460px] md:w-[420px] sm:w-[400px] w-[320px] bg-white pb-4">
       <div className="text-[24px]">
-        {searchKeywordInputValue} 로 검색된 결과
+        {isSearchKeywordInputValue(searchKeywordInputValue)}
       </div>
       <div className="overflow-y-scroll w-full p-2">
         <div className="gap-4 flex flex-col w-full">
-          {searchKeywordArray &&
-            searchKeywordArray.map((keyword, idx) => {
-              return <ResultCard key={keyword.id} data={keyword} />;
-            })}
+          {isSearchKeywordArray(searchKeywordArray)}
         </div>
       </div>
     </div>
