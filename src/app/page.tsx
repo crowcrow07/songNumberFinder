65c0a5e6fbd0909a431763e1cbf6aa82d288a1ae
crowcrow07 <skathd3402@gmail.com>
2024-05-08 17:50:19 +0900

    feat : 화면 스크롤바 생기는 최소 높이 지정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ResultsBox from "@/components/results_box";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen overflow-y-scroll flex justify-center bg-[#ebebeb]">
+    <main className="w-screen h-screen flex justify-center bg-[#ebebeb] max-screen:overflow-y-scroll">
       <div className="xl:w-[1280px] lg:w-[1024px] md:w-[768px] sm:w-[640px] w-[80%] flex gap-16 flex-col items-center justify-center min-h-[660px] h-screen">
         <Header />
         <ResultsBox />
