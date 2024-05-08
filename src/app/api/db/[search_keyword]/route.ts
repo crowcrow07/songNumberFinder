032c1d3b2d449406a5238cf1c1a4b075eb5a401c
crowcrow07 <skathd3402@gmail.com>
2024-05-08 16:09:25 +0900

    feat : 쿼리문 수정

--- src/app/api/db/[search_keyword]/route.ts
+++ src/app/api/db/[search_keyword]/route.ts
@@ -7,8 +7,21 @@ type Params = {
 
 export async function GET(req: Request, context: { params: Params }) {
   try {
-    const searchKeyword = decodeURIComponent(context.params.search_keyword);
-    const query = `SELECT * FROM TJ t WHERE REGEXP_REPLACE(title, '\\s+', '') REGEXP REGEXP_REPLACE('${searchKeyword}', '\\s+', '');`;
+    const searchKeyword = decodeURIComponent(
+      context.params.search_keyword
+    ).replace(/\s+/g, "");
+    const query = `
+        SELECT DISTINCT * FROM TJ t
+        WHERE REGEXP_REPLACE(title, '\\s+', '') REGEXP REGEXP_REPLACE('${searchKeyword}', '\\s+', '')
+        ORDER BY
+          CASE
+            WHEN title = '${searchKeyword}' THEN 1 
+            WHEN title LIKE '${searchKeyword}%' THEN 2 
+            WHEN title LIKE '%${searchKeyword}%' THEN 3 
+            ELSE 4
+          END
+          LIMIT 100
+      `;
 
     const results = await searchDbExecuteQuery(query);
 
