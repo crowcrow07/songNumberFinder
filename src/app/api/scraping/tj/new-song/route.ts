44090bb2feaaec57a3860b50063d40e2150bf6ad
crowcrow07 <skathd3402@gmail.com>
2024-05-09 12:22:31 +0900

    fixed : type name convention

--- src/app/api/scraping/tj/new-song/route.ts
+++ src/app/api/scraping/tj/new-song/route.ts
@@ -2,18 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 
 import getDriver from "../../getDriver";
 import fetchHtml from "../../fetchHtml";
-import parseItems from "../tj_parse_items";
+import { parseTjItems } from "../../parseItems";
 
-import { SongRecord } from "@/app/types/type";
+import { TjSongRecord } from "@/app/types/type";
 
 export async function GET(req: NextRequest) {
   const { browser, page } = await getDriver();
-  const results: SongRecord[] = [];
+  const results: TjSongRecord[] = [];
 
   const url = "https://www.tjmedia.com/tjsong/song_monthNew.asp";
 
   const html = await fetchHtml(page, url);
-  const items = parseItems(html);
+  const items = parseTjItems(html);
 
   results.push(...items);
 
