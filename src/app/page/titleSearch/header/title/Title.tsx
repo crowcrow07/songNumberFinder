df53091046f078cb427207dca8b086fb81b8c67a
crowcrow07 <skathd3402@gmail.com>
2024-05-13 17:46:47 +0900

    style : 스타일 수정

--- src/app/page/titleSearch/header/title/Title.tsx
+++ src/app/page/titleSearch/header/title/Title.tsx
@@ -1,10 +1,10 @@
 export default function Title() {
   return (
     <div className="text-center">
-      <h1 className="font-bold xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl mb-4 text-black">
+      <h1 className="font-bold xl:text-4xl lg:text-3xl md:text-2xl text-xl mb-4 text-black">
         이 노래 노래방에 있을까?
       </h1>
-      <h2 className="xl:text-xl lg:text-lg md:text-base text-sm mb-[25px] text-[#d9d9d9]">
+      <h2 className="xl:text-xl lg:text-lg md:text-base text-sm mb-[15px] text-[#d9d9d9]">
         노래방 번호 검색기
       </h2>
     </div>
