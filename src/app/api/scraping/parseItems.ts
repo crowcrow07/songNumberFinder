b55453b99be8339b35296669d2b03988e0f1b473
crowcrow07 <skathd3402@gmail.com>
2024-05-13 17:47:22 +0900

    feat : ky scraping parseitems

--- src/app/api/scraping/parseItems.ts
+++ src/app/api/scraping/parseItems.ts
@@ -27,13 +27,13 @@ export function parseKyItems(html: string) {
   const $ = cheerio.load(html);
   const items: KySongRecord[] = [];
 
-  $("#index_search_frm > div > ul").each((index, element) => {
+  $("#search_chart_frm_2 > div > ul").each((index, element) => {
     if (index > 0) {
       const li = $(element).find("li");
       const item = {
         songNumber: $(li[1]).text().trim(),
-        title: $(li[2]).text().trim(),
-        artist: $(li[3]).text().trim(),
+        title: $(li[2]).find("span").eq(0).text().trim(),
+        artist: $(li[2]).find("span").eq(1).text().trim(),
       };
       items.push(item);
     }
